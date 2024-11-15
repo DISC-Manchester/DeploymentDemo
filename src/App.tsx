1e165833c3fff17b8c055bd79898e4ab0317c5cb
Sam Hirst <SamuelJHirst@outlook.com>
2024-11-15 14:35:03 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'center',
+      }}
+    >
+      <h1
+        style={{
+          fontFamily: 'sans-serif',
+          fontSize: 48,
+        }}
+      >
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
